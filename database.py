71b2b5edcbd0dd119969285ba53af8216291c467
DataWorshipper <mandalabhiraj04@gmail.com>
2026-04-15 09:44:41 +0530

    added database setup

---
+++ database.py
@@ -0,0 +1,8 @@
+import os
+db_url = os.getenv("DATABASE_URI")
+from psycopg_pool import ConnectionPool
+connection_pool = ConnectionPool(
+    conninfo=db_url,
+    max_size=20,
+    kwargs={"autocommit": True} 
+)
